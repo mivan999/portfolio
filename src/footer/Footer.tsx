38d15ae081d22ebe815023929658a689c077ea5e
Ivan Mulyavin <mivan999@mail.ru>
2022-02-08 14:06:18 +0700

    footer add

---
+++ src/footer/Footer.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './Footer.module.css'
+import styleContainer from '../common/style/Container.module.css';
+
+
+const Footer = () => {
+    return (
+        <div className={styles.footerBlock}>
+
+            <div
+                className={`${styleContainer.container} ${styles.footerContainer}`}>
+                <h2> Footer</h2>
+                <div className={styles.footer}>
+                  <div>  footer footer footer</div>
+                  <div>  footer footer footer</div>
+                  <div>  footer footer footer</div>
+
+                </div>
+                <div> Все права защищены 2022 </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
--- src/nav/Nav.tsx
+++ src/nav/Nav.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import style from './Nav.module.css'
+import styleContainer from './../common/style/Container.module.css'
+
 const Nav = () => {
     return (
-        <div className={style.nav}>
-            <a href='/'>Главная</a>
-            <a href='/'>Скилы</a>
-            <a href='/'>Проекты</a>
-            <a href='/'>Контакты</a>
+        <div className={styleContainer.container}>
+            <div className={`${style.nav} `}>
+                <a href="/">Главная</a>
+                <a href="/">Скилы</a>
+                <a href="/">Проекты</a>
+                <a href="/">Контакты</a>
+            </div>
         </div>
     );
 };
